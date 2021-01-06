ca0033edf0cc607568ec8ae12f4e94a6b7212cbf
C1rdec <cedric_lampron@hotmail.com>
2021-01-05 23:17:32 -0500

    Added ReactiveUI

--- src/GitLurker.UI/MainWindow.xaml.cs
+++ src/GitLurker.UI/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-namespace GitLurker.UI
-    using System;
-    using System.Threading.Tasks;
-    using System.Windows.Documents;
-    using System.Windows.Navigation;
-    {
-        using System.Windows.Controls;
-        using System.Windows.Media;
-        
-            using System.Linq;
+﻿namespace GitLurker.UI
+{
+    using System.Windows;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,7 +9,7 @@ namespace GitLurker.UI
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
