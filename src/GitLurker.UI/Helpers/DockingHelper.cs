77d91a22073a3b17665ef3339714ccfd8441208c
C1rdec <cedric_lampron@hotmail.com>
2022-02-18 01:39:33 -0500

    Move pinvok into native file

--- src/GitLurker.UI/Helpers/DockingHelper.cs
+++ src/GitLurker.UI/Helpers/DockingHelper.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
@@ -8,31 +6,18 @@ namespace GitLurker.UI.Helpers
 {
     internal class DockingHelper
     {
-        [DllImport("User32.dll")]
-        public static extern int SetForegroundWindow(IntPtr point);
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetForegroundWindow();
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr SetActiveWindow(IntPtr hWnd);
-
         public static void SetForeground(Window window, Action callback)
         {
             var handle = new WindowInteropHelper(window).Handle;
 
-            var foreground = GetForegroundWindow();
+            var foreground = Native.GetForegroundWindow();
 
             if (handle == foreground)
             {
                 callback();
             }
 
-            var result = SetForegroundWindow(handle);
-            Debug.WriteLine("SetForeground");
+            Native.SetForegroundWindow(handle);
         }
     }
 }
