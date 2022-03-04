7da936032bf720f9a7fe6673e2c30047d880bcfd
C1rdec <cedric_lampron@hotmail.com>
2022-03-04 00:48:49 -0500

    Add the ability to add WorkSpace

--- src/GitLurker.UI/ViewModels/RepoManagerViewModel.cs
+++ src/GitLurker.UI/ViewModels/RepoManagerViewModel.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Forms;
+using Caliburn.Micro;
 using GitLurker.Models;
 
 namespace GitLurker.UI.ViewModels
 {
     public class RepoManagerViewModel
     {
+        #region Fields
+
         private SettingsFile _settings;
+
+        #endregion
+
+        #region Constructors
+
 
         public RepoManagerViewModel(SettingsFile settings)
         {
@@ -18,6 +27,32 @@ namespace GitLurker.UI.ViewModels
             }
         }
 
+        #endregion
+
+        #region Properties
+
         public ObservableCollection<FolderViewModel> Folders { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public void Add()
+        {
+            using var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            var settings = IoC.Get<SettingsFile>();
+            settings.Entity.Workspaces.Add(dialog.SelectedPath);
+            settings.Save();
+
+            Folders.Add(new FolderViewModel(dialog.SelectedPath));
+        }
+
+        #endregion
     }
 }
