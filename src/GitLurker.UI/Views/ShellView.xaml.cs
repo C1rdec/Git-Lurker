eca32f8d07e400c347d793b764bf50c0dc0adaf0
Cedric Lampron <cedric.lampron@sonepar.ca>
2022-03-04 10:12:30 -0500

    Remove Shellview round borders

--- src/GitLurker.UI/Views/ShellView.xaml.cs
+++ src/GitLurker.UI/Views/ShellView.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Interop;
+﻿using System.Windows;
 
 namespace GitLurker.UI.Views
 {
@@ -10,39 +7,6 @@ namespace GitLurker.UI.Views
         public ShellView()
         {
             InitializeComponent();
-
-            IntPtr hWnd = new WindowInteropHelper(GetWindow(this)).EnsureHandle();
-            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
-            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
-            DwmSetWindowAttribute(hWnd, attribute, ref preference, sizeof(uint));
-        }
-
-        public enum DWMWINDOWATTRIBUTE
-        {
-            DWMWA_WINDOW_CORNER_PREFERENCE = 33
-        }
-
-        // The DWM_WINDOW_CORNER_PREFERENCE enum for DwmSetWindowAttribute's third parameter, which tells the function
-        // what value of the enum to set.
-        public enum DWM_WINDOW_CORNER_PREFERENCE
-        {
-            DWMWCP_DEFAULT = 0,
-            DWMWCP_DONOTROUND = 1,
-            DWMWCP_ROUND = 2,
-            DWMWCP_ROUNDSMALL = 3
-        }
-
-        // Import dwmapi.dll and define DwmSetWindowAttribute in C# corresponding to the native function.
-        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern long DwmSetWindowAttribute(IntPtr hwnd,
-                                                         DWMWINDOWATTRIBUTE attribute,
-                                                         ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
-                                                         uint cbAttribute);
-
-        protected override void OnActivated(EventArgs e)
-        {
-            base.OnActivated(e);
-            SearchTerm.Focus();
         }
     }
 }
