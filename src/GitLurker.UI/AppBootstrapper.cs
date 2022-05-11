4138d0a5d466db938997db37aa7b9e2b6f7edce6
C1rdec <cedric_lampron@hotmail.com>
2022-05-11 12:06:04 -0400

    Remove ActionBarViewModel from the container

--- src/GitLurker.UI/AppBootstrapper.cs
+++ src/GitLurker.UI/AppBootstrapper.cs
@@ -59,7 +59,6 @@
             _container.Singleton<KeyboardService, KeyboardService>();
             _container.Singleton<IWindowManager, WindowManager>();
             _container.Singleton<IEventAggregator, EventAggregator>();
-            _container.PerRequest<ActionBarViewModel, ActionBarViewModel>();
             _container.PerRequest<ShellViewModel, ShellViewModel>();
 
             var settings = new SettingsFile();
