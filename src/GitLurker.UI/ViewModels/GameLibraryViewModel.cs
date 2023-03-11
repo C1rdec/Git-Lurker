3848ee9d7ef35260388be8c2fe00910b0596743b
C1rdec <cedric_lampron@hotmail.com>
2023-03-11 18:24:02 -0500

    Change EOF to CRLF

--- src/GitLurker.UI/ViewModels/GameLibraryViewModel.cs
+++ src/GitLurker.UI/ViewModels/GameLibraryViewModel.cs
@@ -43,7 +43,7 @@ namespace GitLurker.UI.ViewModels
         public GameViewModel SelectedGameViewModel
         {
             get
-            {
+            {   
                 return _selectedGameViewModel;
             }
 
