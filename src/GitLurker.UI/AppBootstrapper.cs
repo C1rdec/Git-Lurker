fd81c486b9fa81faf44b31cc102568647fa44a72
C1rdec <cedric_lampron@hotmail.com>
2023-04-16 09:07:31 -0400

    Update caliburn

--- src/GitLurker.UI/AppBootstrapper.cs
+++ src/GitLurker.UI/AppBootstrapper.cs
@@ -50,7 +50,7 @@
                 return;
             }
 
-            DisplayRootViewFor<ShellViewModel>();
+            DisplayRootViewForAsync<ShellViewModel>();
         }
 
         /// <summary>
